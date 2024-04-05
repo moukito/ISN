fcb08a62be88b94d8ab320ef04e79d84e9f832b4
EnyoYoen <enyotoyrep@gmail.com>
2024-04-05 18:30:51 +0200

    Add Farm building and the possibility to update build

--- src/model/Structures.py
+++ src/model/Structures.py
@@ -1,7 +1,7 @@
 from enum import Enum
 import random
 
-from Geometry import Point
+from Geometry import Point, Rectangle
 
 class StructureType(Enum):
     ORE = 1
@@ -69,20 +69,47 @@ class BuildingType(Enum):
     BASE_CAMP = 1
     FARM = 2
 
+class BuildingState(Enum):
+    PLACED = 0
+    BUILDING = 1
+    BUILT = 2
+
 # TODO: maybe create a class 'TypedStructure' inheriting of 'Structure' to add the type attribute instead of adding it to every subclass of Structure?
 class Building(Structure):
-    __slots__ = ["type", "costs"]
+    __slots__ = ["type", "costs", "health", "building_duration", "building_time", "workers", "state"]
 
-    def __init__(self, costs, type, coords, points, orientation=Orientation.RANDOM) -> None:
+    def __init__(self, costs, health, building_duration, type, coords, points, orientation=Orientation.RANDOM) -> None:
         super().__init__(coords, points, orientation)
-        self.type = type
         self.costs = costs
+        self.health = health
+        self.building_duration = building_duration
+        self.building_time = 0
+        self.workers = 0
+        self.type = type
+        self.state = BuildingState.PLACED
+
+    def addWorkers(self, workers_number):
+        self.workers += workers_number
+
+    def update(self, duration):
+        # Should be called BEFORE adding or removing workers
+        old_time = self.building_time
+        self.building_time += duration * self.workers
+
+        if self.building_time != 0 and old_time == 0:
+            self.state = BuildingState.BUILDING
+
+        if self.building_duration < self.building_time:
+            self.state = BuildingState.BUILT
+
+        return self.state
 
 class BaseCamp(Building):
     def __init__(self, coords, orientation=Orientation.RANDOM) -> None:
         # TODO: add costs when the ressource system is ready
-        super().__init__(None, BuildingType.BASE_CAMP, coords,
-                         [Point(-1, -1), Point(0, -1), Point(1, -1),
-                          Point(-1, 0),  Point(0, 0),  Point(1, 0), 
-                          Point(-1, 1),  Point(0, 1),  Point(1, 1)], orientation)
+        super().__init__(None, 0, BuildingType.BASE_CAMP, coords, Rectangle(-2, -2, 2, 2).toPointList(), orientation)
         
+class Farm(Building):
+    def __init__(self, coords, orientation=Orientation.RANDOM) -> None:
+        # TODO: add costs when the ressource system is ready
+        super().__init__(None, 2 * 60, BuildingType.FARM, coords, Rectangle(-1, -1, 1, 1).toPointList(), orientation)
