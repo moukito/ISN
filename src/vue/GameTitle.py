6b9d7b00b0e438a7c8eec8c3d8ffd8cb2a596542
moukito <theofremont50@gmail.com>
2024-04-10 11:19:40 +0200

    Init default methods

--- src/vue/GameTitle.py
+++ src/vue/GameTitle.py
@@ -3,3 +3,12 @@ from src.vue.Scene import Scene
 
 class GameTitle(Scene):
     __slots__ = []
+
+    def handle_events(self):
+        pass
+
+    def update(self):
+        pass
+
+    def render(self):
+        pass # TODO blit background and write doc
