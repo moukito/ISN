f2fd514a76aae3de86b6b2fb910924b794a2a68e
moukito <theofremont50@gmail.com>
2024-04-10 11:19:23 +0200

    Unifying working directory to root project directory

--- src/main.py
+++ src/main.py
@@ -1,9 +1,21 @@
+import os
+import sys
+
 from vue.Core import Core
 
 
 def exodus():
+    verifying_path()
     game = Core()
     game.run()
+
+
+def verifying_path():
+    path = sys.argv[0]
+    keyword = path.split("/")
+    path = path[:-(len(keyword[-1]) + len(keyword[-2]) + 2)]
+    if path != os.getcwd():
+        os.chdir(path)
 
 
 if __name__ == "__main__":
