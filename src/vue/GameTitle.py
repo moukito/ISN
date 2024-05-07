c6d4eaeea8f4d9d9fed09f973467aadc695bb9a4
moukito <theofremont50@gmail.com>
2024-05-07 22:51:18 +0200

    final state of game title

--- src/vue/GameTitle.py
+++ src/vue/GameTitle.py
@@ -1,5 +1,6 @@
 import pygame
 
+from src.vue.Button import Button
 from src.vue.Choice import Choice
 from src.vue.Scene import Scene
 from src.vue.SettingScene import SettingsScene
@@ -17,7 +18,7 @@ class GameTitle(Scene):
             render(): Renders the title screen.
     """
 
-    __slots__ = ["bg", "choice", "options", "font"]
+    __slots__ = ["bg", "choice", "options", "font", "buttons"]
 
     def __init__(self, core):
         """
@@ -36,7 +37,14 @@ class GameTitle(Scene):
         self.choice = Choice()
         self.options = ["jouer", "option", "quitter"]
         font_size = int(self.screen.get_height() * 0.065)
-        self.font = pygame.font.Font("asset/font/Space-Laser-BF65f80ab15c082.otf", font_size)
+
+        button_width = self.screen.get_width() * 0.156
+        button_height = self.screen.get_height() * 0.062
+        self.buttons = [Button(option, (self.screen.get_width() - button_width) // 2,
+                               int(self.screen.get_height() * 0.65) + i * (self.screen.get_height() * 0.1),
+                               button_width, button_height,
+                               (255, 255, 255), "asset/font/Space-Laser-BF65f80ab15c082.otf", font_size) for i, option
+                        in enumerate(self.options)]
 
         self.run()
 
@@ -48,7 +56,7 @@ class GameTitle(Scene):
             Handles events specific to the title screen.
         """
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
+            if event.key == pygame.K_ESCAPE or event.key == pygame.K_KP_ENTER:
                 pygame.event.post(pygame.event.Event(pygame.QUIT))
             elif event.key == pygame.K_UP:
                 self.choice -= 1
@@ -59,10 +67,26 @@ class GameTitle(Scene):
                 if choice == 1:
                     pass
                 elif choice == 2:
-                    settings_scene = SettingsScene(self)  # Create and run the settings scene
+                    settings_scene = SettingsScene(self.core)  # Create and run the settings scene
                     settings_scene.run()
                 elif choice == 3:
                     pygame.event.post(pygame.event.Event(pygame.QUIT))
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            for i, button in enumerate(self.buttons):
+                if button.is_clicked(event):
+                    if i == 0:
+                        pass
+                    elif i == 1:
+                        settings_scene = SettingsScene(self.core)  # Create and run the settings scene
+                        settings_scene.run()
+                    elif i == 2:
+                        pygame.event.post(pygame.event.Event(pygame.QUIT))
+        elif event.type == pygame.MOUSEMOTION:
+            for i, button in enumerate(self.buttons):
+                if button.is_hovered(event):
+                    self.choice.set_choice(i + 1)
+
+    # TODO : merge button actions
 
     def update(self):
         """
@@ -86,28 +110,26 @@ class GameTitle(Scene):
         self.screen.blit(self.bg, (0, 0))  # Draw the background image onto the screen
 
         choice = self.choice.get_choice()
-        for i, option in enumerate(self.options):  # Iterate over the list of options
+        for i, button in enumerate(self.buttons):  # Iterate over the list of options
             if choice == i + 1:  # Check if the current option is the selected choice
-                color = (196, 158, 33)  # If it is, set the color to yellow
+                button.color = (196, 158, 33)  # If it is, set the color to yellow
             else:
-                color = (1, 6, 138)  # If it's not, set the color to blue
-            text = self.font.render(option, 1, color)  # Render the option as a text object with the determined color
+                button.color = (1, 6, 138)  # If it's not, set the color to blue
+            button.render(self.screen)  # Render the button object onto the screen
 
             # Calculate the position of the text
-            text_width, text_height = text.get_size()  # Get the size of the text object
+            text_width, text_height = button.get_size()  # Get the size of the text object
             x = (self.screen.get_width() - text_width) // 2  # Calculate the horizontal position (centered)
-            y = int(
-                self.screen.get_height() * 0.65) + i * 150  # Calculate the vertical position (80% from the top plus an offset)
-            position = (x, y)  # Create a tuple for the position
-
-            self.screen.blit(text, position)  # Draw the text object onto the screen at the calculated position
+            y = int(self.screen.get_height() * 0.65) + i * (
+                        self.screen.get_height() * 0.1)  # Calculate the vertical position (80% from the top plus an offset)
 
             # Draw a triangle around the current choice
             if choice == i + 1:
-                offset = 0.8 * text_height / 2
+                offset = text_height / 2
                 x -= 20
                 points = [(x - offset, y + offset * 2), (x - offset, y), (x, y + offset)]
-                pygame.draw.polygon(self.screen, color, points)
+                pygame.draw.polygon(self.screen, button.color, points)
                 x += 40 + text_width
                 points = [(x + offset, y + offset * 2), (x + offset, y), (x, y + offset)]
-                pygame.draw.polygon(self.screen, color, points)
+                pygame.draw.polygon(self.screen, button.color, points)
+                # TODO : rewrite correctly this part
