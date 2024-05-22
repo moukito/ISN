faa3719e929d1aa0d2cec8620aa1e512ed8b982f
moukito <theofremont50@gmail.com>
2024-05-22 16:48:14 +0200

    cancel format

--- src/model/Map.py
+++ src/model/Map.py
@@ -17,42 +17,21 @@ class Biomes(Enum):
     DESOLATED_FOREST = 5
     SNOW = 6
 
-
 class Map:
-    __slots__ = [
-        "perlin",
-        "map_chunks",
-        "ores",
-        "buildings",
-        "structures",
-        "occupied_coords",
-        "chunk_occupied_coords",
-        "building_type",
-    ]
+    __slots__ = ["perlin", "map_chunks", "ores", "buildings", "structures", "occupied_coords", "chunk_occupied_coords"]
 
-    def __init__(self, seed=1) -> None:
+    def __init__(self, seed = 1) -> None:
         self.perlin = Perlin(seed, 4, 2, 1, 50, 1)
-        # self.perlin = Perlin(seed, 4, 2, 2, 100, 1)
+        #self.perlin = Perlin(seed, 4, 2, 2, 100, 1)
         self.map_chunks = {}
-        self.ores = (
-            {}
-        )  # {Point (chunk coords): {OreType: Point}} TODO : Maybe change to {Point (chunk coords): {OreType: Ore}}
+        self.ores = {} # {Point (chunk coords): {OreType: Point}} TODO : Maybe change to {Point (chunk coords): {OreType: Ore}}
         self.buildings = []
-        self.building_type = {}  # {StructureType: Structure}
-        self.structures = {}  # {Point (chunk coords): [Structure]}
-        self.occupied_coords = {}  # {Point: Structure}
-        self.chunk_occupied_coords = {}  # {Point (chunk coords): [Point]}
+        self.building_type = {} # {StructureType: Structure}
+        self.structures = {} # {Point (chunk coords): [Structure]}
+        self.occupied_coords = {} # {Point: Structure}
+        self.chunk_occupied_coords = {} # {Point (chunk coords): [Point]}
 
-    def try_generate_ore(
-            self,
-            chunk_coords,
-            position,
-            treshold,
-            search_area_size,
-            search_ores,
-            ores_count_treshold,
-            ore_type,
-    ):
+    def try_generate_ore(self, chunk_coords, position, treshold, search_area_size, search_ores, ores_count_treshold, ore_type):
         if random.random() < treshold:
             ores_count = 0
             values = [0]
@@ -60,22 +39,16 @@ class Map:
                 values.append(-i)
                 values.append(i)
             for i in range(search_area_size * search_area_size):
-                chunk_ores = self.ores.get(
-                    Point(
-                        chunk_coords.x + values[i // search_area_size],
-                        chunk_coords.y + values[i % search_area_size],
-                    ),
-                    None,
-                )
-                if chunk_ores != None:
+                chunk_ores = self.ores.get(Point(chunk_coords.x + values[i // search_area_size], chunk_coords.y + values[i % search_area_size]), None)
+                if chunk_ores is not None:
                     for search_ore in search_ores:
                         actual_chunk_ores = chunk_ores.get(search_ore, None)
-                        if actual_chunk_ores != None:
+                        if actual_chunk_ores is not None:
                             ores_count += len(actual_chunk_ores)
             if ores_count < ores_count_treshold:
-                if self.ores.get(chunk_coords, None) == None:
+                if self.ores.get(chunk_coords, None) is None:
                     self.ores[chunk_coords] = {}
-                if self.ores[chunk_coords].get(ore_type, None) == None:
+                if self.ores[chunk_coords].get(ore_type, None) is None:
                     self.ores[chunk_coords][ore_type] = []
 
                 ore = Ore(ore_type, position)
@@ -83,12 +56,7 @@ class Map:
                 found = False
                 i = 0
                 while not found and i < len(ore.points):
-                    if (
-                            self.occupied_coords.get(
-                                ore.points[i] + absolute_chunk_origin, None
-                            )
-                            is not None
-                    ):
+                    if self.occupied_coords.get(ore.points[i] + absolute_chunk_origin, None) is not None:
                         found = True
                     i += 1
 
@@ -96,9 +64,7 @@ class Map:
                     self.ores[chunk_coords][ore_type].append(position)
                     # TODO: maybe remove the code below in favor of self.ores?
                     for point in ore.points:
-                        self.occupied_coords[
-                            absolute_chunk_origin + position + point
-                        ] = ore
+                        self.occupied_coords[absolute_chunk_origin + position + point] = ore
 
     def process_chunk(self, chunk_data, chunk_coords):
         processed_chunk = np.empty((Perlin.CHUNK_SIZE, Perlin.CHUNK_SIZE))
@@ -108,50 +74,18 @@ class Map:
                 position = Point(i, j)
                 if height > 3.5:
                     processed_chunk[i][j] = Biomes.SNOW.value
-                    self.try_generate_ore(
-                        chunk_coords,
-                        position,
-                        0.005,
-                        3,
-                        [OreType.CRYSTAL],
-                        3,
-                        OreType.CRYSTAL,
-                    )
+                    self.try_generate_ore(chunk_coords, position, 0.005, 3, [OreType.CRYSTAL], 3, OreType.CRYSTAL)
                 elif height > 2:
                     processed_chunk[i][j] = Biomes.DESOLATED_FOREST.value
-                    self.try_generate_ore(
-                        chunk_coords,
-                        position,
-                        0.01,
-                        1,
-                        [OreType.COPPER],
-                        2,
-                        OreType.COPPER,
-                    )
+                    self.try_generate_ore(chunk_coords, position, 0.01, 1, [OreType.COPPER], 2, OreType.COPPER)
                 elif height > 1:
                     processed_chunk[i][j] = Biomes.MUSHROOM_FOREST.value
                 elif height > -2:
                     processed_chunk[i][j] = Biomes.PLAIN.value
-                    self.try_generate_ore(
-                        chunk_coords,
-                        position,
-                        0.015,
-                        1,
-                        [OreType.IRON],
-                        2,
-                        OreType.IRON,
-                    )
+                    self.try_generate_ore(chunk_coords, position, 0.015, 1, [OreType.IRON], 2, OreType.IRON)
                 elif height > -3:
                     processed_chunk[i][j] = Biomes.VOLCANO.value
-                    self.try_generate_ore(
-                        chunk_coords,
-                        position,
-                        0.005,
-                        5,
-                        [OreType.VULCAN],
-                        1,
-                        OreType.VULCAN,
-                    )
+                    self.try_generate_ore(chunk_coords, position, 0.005, 5, [OreType.VULCAN], 1, OreType.VULCAN)
                 else:
                     processed_chunk[i][j] = Biomes.LAVA.value
         return processed_chunk
@@ -171,19 +105,16 @@ class Map:
             for j in range(height):
                 actual_chunk_coords = Point(chunk_coords.x + i, chunk_coords.y + j)
                 chunks.append(self.get_chunk(actual_chunk_coords))
-            rows.append(np.concatenate(chunks, axis=1))
+            rows.append(np.concatenate(chunks, axis = 1))
             chunks.clear()
 
-        return np.concatenate(rows, axis=0)
+        return np.concatenate(rows, axis = 0)
 
     def try_place_structure(self, structure):
         center = structure.coords
         i = 0
         l = len(structure.points)
-        while (
-                i < l
-                and self.occupied_coords.get(center + structure.points[i], None) is None
-        ):
+        while i < l and self.occupied_coords.get(center + structure.points[i], None) is None:
             i += 1
 
         return i == l
@@ -214,4 +145,4 @@ class Map:
 
     def update(self, duration):
         for building in self.buildings:
-            building.update(duration)
+           building.update(duration)
--- src/vue/GameVue.py
+++ src/vue/GameVue.py
@@ -16,35 +16,7 @@ from model.Saver import Saver
 
 
 class GameVue(Scene):
-    __slots__ = [
-        "saver",
-        "player",
-        "map",
-        "actual_chunks",
-        "buildings",
-        "camera_pos",
-        "clicking",
-        "camera_moved",
-        "start_click_pos",
-        "mouse_pos",
-        "building_moved",
-        "building",
-        "building_pos",
-        "building_pos_old",
-        "cell_pixel_size",
-        "screen_width",
-        "screen_height",
-        "cell_width_count",
-        "cell_height_count",
-        "ressource_font",
-        "ressource_icons",
-        "ressource_background",
-        "ressource_background_size",
-        "biomes_textures",
-        "colors",
-        "clock",
-        "last_timestamp",
-    ]
+    __slots__ = ["saver", "player", "map", "actual_chunks", "buildings", "camera_pos", "clicking", "camera_moved", "start_click_pos", "mouse_pos", "building_moved", "building", "building_pos", "building_pos_old", "cell_pixel_size", "screen_width", "screen_height", "cell_width_count", "cell_height_count", "ressource_font", "ressource_icons", "ressource_background", "ressource_background_size", "biomes_textures", "colors", "clock", "last_timestamp"]
 
     def __init__(self, core):
         super().__init__(core)
@@ -77,10 +49,7 @@ class GameVue(Scene):
         self.reset_building()
 
         self.cell_pixel_size = 30
-        self.screen_width, self.screen_height = (
-            self.screen.get_width(),
-            self.screen.get_height(),
-        )
+        self.screen_width, self.screen_height = self.screen.get_width(), self.screen.get_height()
         self.cell_width_count = ceil(self.screen_width / self.cell_pixel_size)
         self.cell_height_count = ceil(self.screen_height / self.cell_pixel_size)
 
@@ -101,31 +70,16 @@ class GameVue(Scene):
 
         self.ressource_icons = {}
         for ressource_type in RessourceType:
-            self.ressource_icons[ressource_type] = pygame.transform.scale(
-                pygame.image.load(
-                    "assets/icons/" + ressource_type.name.lower() + ".png"
-                ).convert_alpha(),
-                (26, 26),
-            )
-        # print(self.ressource_icons.keys())
+            self.ressource_icons[ressource_type] = pygame.transform.scale(pygame.image.load("assets/icons/" + ressource_type.name.lower() + ".png").convert_alpha(), (26, 26))
+        #print(self.ressource_icons.keys())
 
         self.biomes_textures = {}
         for biome in Biomes:
-            self.biomes_textures[biome] = pygame.transform.scale(
-                pygame.image.load(
-                    "assets/icons/" + biome.name.lower() + ".jpg"
-                ).convert_alpha(),
-                (self.cell_pixel_size, self.cell_pixel_size),
-            )
+            self.biomes_textures[biome] = pygame.transform.scale(pygame.image.load("assets/icons/" + biome.name.lower() + ".jpg").convert_alpha(), (self.cell_pixel_size, self.cell_pixel_size))
 
-        self.ressource_background = pygame.transform.scale(
-            pygame.image.load("assets/ui.png").convert_alpha(), (312, 202)
-        )
-        self.ressource_background_size = (
-            self.ressource_background.get_width(),
-            self.ressource_background.get_height(),
-        )
-        # self.ressource_background = pygame.image.load("assets/ui.png").convert_alpha()
+        self.ressource_background = pygame.transform.scale(pygame.image.load("assets/ui.png").convert_alpha(), (312, 202))
+        self.ressource_background_size = (self.ressource_background.get_width(), self.ressource_background.get_height())
+        #self.ressource_background = pygame.image.load("assets/ui.png").convert_alpha()
 
         self.colors = {
             Colors.BLACK: pygame.Color(pygame.color.THECOLORS["black"]),
@@ -136,7 +90,7 @@ class GameVue(Scene):
             Colors.BLUEVIOLET: pygame.Color(pygame.color.THECOLORS["blueviolet"]),
             Colors.RED: pygame.Color(pygame.color.THECOLORS["red"]),
             Colors.ORANGE: pygame.Color(pygame.color.THECOLORS["orange"]),
-            Colors.YELLOW: pygame.Color(pygame.color.THECOLORS["yellow"]),
+            Colors.YELLOW: pygame.Color(pygame.color.THECOLORS["yellow"])
         }
 
     def handle_events(self, event):
@@ -153,27 +107,21 @@ class GameVue(Scene):
                 if self.building is None:
                     self.clicking = False
                 else:
-                    self.building.coords = (
-                                                   self.building_pos
-                                                   + Point(int(self.camera_pos.x), int(self.camera_pos.y))
-                                                   - Point(self.screen_width, self.screen_height) // 2
-                                           ) // self.cell_pixel_size
+                    self.building.coords = (self.building_pos + Point(int(self.camera_pos.x), int(self.camera_pos.y)) - Point(self.screen_width, self.screen_height) // 2) // self.cell_pixel_size
                     result = self.map.place_structure(self.building)
                     if result:
                         self.reset_building()
         elif event.type == pygame.MOUSEMOTION:
-            pos = pygame.mouse.get_pos()
+            pos  = pygame.mouse.get_pos()
             pos_point = Point(pos[0], pos[1])
             if self.clicking:
-                pos = pygame.mouse.get_pos()
+                pos  = pygame.mouse.get_pos()
                 pos_point = Point(pos[0], pos[1])
                 # TODO: fine-tune this
                 self.camera_pos += (self.mouse_pos - pos_point) * (2 / 2.0)
                 self.camera_moved = (self.mouse_pos - pos_point) != Point.origin()
             elif self.building is not None:
-                self.building_moved = (
-                                              self.mouse_pos - self.building_pos
-                                      ) // self.cell_pixel_size != Point.origin()
+                self.building_moved = (self.mouse_pos - self.building_pos) // self.cell_pixel_size != Point.origin()
                 if self.building_moved:
                     self.building_pos = pos_point
             self.mouse_pos = pos_point
@@ -181,11 +129,7 @@ class GameVue(Scene):
             if event.key == pygame.K_p:
                 if self.building is None:
                     self.building_moved = True
-                    self.building = Farm(
-                        Point(int(self.camera_pos.x), int(self.camera_pos.y))
-                        // self.cell_pixel_size,
-                        self.player,
-                    )
+                    self.building = Farm(Point(int(self.camera_pos.x), int(self.camera_pos.y)) // self.cell_pixel_size, self.player)
                     self.building_pos_old = self.building_pos
                     self.building_pos = self.mouse_pos
                 else:
@@ -193,8 +137,6 @@ class GameVue(Scene):
                     self.building_moved = True
             if event.key == pygame.K_s:
                 self.saver.save_map(self.map)
-            elif event.key == pygame.K_ESCAPE:
-                self.running = False
 
     def update(self):
         timestamp = time_ns()
@@ -215,9 +157,9 @@ class GameVue(Scene):
 
             self.render_interface()
 
-        # print(self.clock.get_fps())
+        #print(self.clock.get_fps())
 
-        # self.clock.tick()
+        #self.clock.tick()
 
     def render_map(self):
         camera_pos = Point(int(self.camera_pos.x), int(self.camera_pos.y))
@@ -228,66 +170,25 @@ class GameVue(Scene):
 
         screen_center = Point(self.screen_width // 2, self.screen_height // 2)
         chunk_tl = screen_center - camera_chunk_cell * self.cell_pixel_size
-        chunk_br = (
-                chunk_tl
-                + Point(
-            Perlin.CHUNK_SIZE * self.cell_pixel_size,
-            Perlin.CHUNK_SIZE * self.cell_pixel_size,
-        )
-                - camera_offset
-        )
+        chunk_br = chunk_tl + Point(Perlin.CHUNK_SIZE * self.cell_pixel_size, Perlin.CHUNK_SIZE * self.cell_pixel_size) - camera_offset
 
         margin_tl = Point(max(0, chunk_tl.x), max(0, chunk_tl.y))
-        margin_br = Point(
-            max(0, self.screen_width - chunk_br.x),
-            max(0, self.screen_height - chunk_br.y),
-        )
-        cells_tl = Point(
-            ceil(margin_tl.x / self.cell_pixel_size),
-            ceil(margin_tl.y / self.cell_pixel_size),
-        )
-        cells_br = Point(
-            ceil(margin_br.x / self.cell_pixel_size),
-            ceil(margin_br.y / self.cell_pixel_size),
-        )
-        chunks_tl = Point(
-            ceil(cells_tl.x / Perlin.CHUNK_SIZE), ceil(cells_tl.y / Perlin.CHUNK_SIZE)
-        )
-        chunks_br = Point(
-            ceil(cells_br.x / Perlin.CHUNK_SIZE), ceil(cells_br.y / Perlin.CHUNK_SIZE)
-        )
+        margin_br = Point(max(0, self.screen_width - chunk_br.x), max(0, self.screen_height - chunk_br.y))
+        cells_tl = Point(ceil(margin_tl.x / self.cell_pixel_size), ceil(margin_tl.y / self.cell_pixel_size))
+        cells_br = Point(ceil(margin_br.x / self.cell_pixel_size), ceil(margin_br.y / self.cell_pixel_size))
+        chunks_tl = Point(ceil(cells_tl.x / Perlin.CHUNK_SIZE), ceil(cells_tl.y / Perlin.CHUNK_SIZE))
+        chunks_br = Point(ceil(cells_br.x / Perlin.CHUNK_SIZE), ceil(cells_br.y / Perlin.CHUNK_SIZE))
 
-        cells_tl_signed = Point(
-            chunk_tl.x / self.cell_pixel_size, chunk_tl.y / self.cell_pixel_size
-        )
-        cells_br_signed = Point(
-            (self.screen_width - chunk_br.x) / self.cell_pixel_size,
-            (self.screen_height - chunk_br.y) / self.cell_pixel_size,
-        )
-        cells_size = Point(
-            ceil(cells_tl_signed.x + cells_br_signed.x) + Perlin.CHUNK_SIZE,
-            ceil(cells_tl_signed.y + cells_br_signed.y) + Perlin.CHUNK_SIZE,
-        )
+        cells_tl_signed = Point(chunk_tl.x / self.cell_pixel_size, chunk_tl.y / self.cell_pixel_size)
+        cells_br_signed = Point((self.screen_width - chunk_br.x) / self.cell_pixel_size, (self.screen_height - chunk_br.y) / self.cell_pixel_size)
+        cells_size = Point(ceil(cells_tl_signed.x + cells_br_signed.x) + Perlin.CHUNK_SIZE, ceil(cells_tl_signed.y + cells_br_signed.y) + Perlin.CHUNK_SIZE)
         chunks_size = chunks_br + chunks_tl
 
         ceiled_cells_tl = Point(ceil(cells_tl_signed.x), ceil(cells_tl_signed.y))
-        cell_offset = Point(
-            (
-                -ceiled_cells_tl.x
-                if ceiled_cells_tl.x <= 0
-                else Perlin.CHUNK_SIZE - ceiled_cells_tl.x
-            ),
-            (
-                -ceiled_cells_tl.y
-                if ceiled_cells_tl.y <= 0
-                else Perlin.CHUNK_SIZE - ceiled_cells_tl.y
-            ),
-        )
+        cell_offset = Point(-ceiled_cells_tl.x if ceiled_cells_tl.x <= 0 else Perlin.CHUNK_SIZE - ceiled_cells_tl.x, -ceiled_cells_tl.y if ceiled_cells_tl.y <= 0 else Perlin.CHUNK_SIZE - ceiled_cells_tl.y)
 
         tl_chunk = camera_chunk - chunks_tl
-        chunks = self.map.get_area_around_chunk(
-            camera_chunk - chunks_tl, chunks_size.x + 1, chunks_size.y + 1
-        )
+        chunks = self.map.get_area_around_chunk(camera_chunk - chunks_tl, chunks_size.x + 1, chunks_size.y + 1)
         self.actual_chunks = chunks
 
         # RENDER MAP
@@ -295,13 +196,8 @@ class GameVue(Scene):
             for j in range(0, cells_size.y):
                 x = i + cell_offset.x
                 y = j + cell_offset.y
-                self.screen.blit(
-                    self.biomes_textures[Biomes(int(chunks[x][y]))],
-                    (
-                        i * self.cell_pixel_size - camera_offset.x,
-                        j * self.cell_pixel_size - camera_offset.y,
-                    ),
-                )
+                self.screen.blit(self.biomes_textures[Biomes(int(chunks[x][y]))], (i * self.cell_pixel_size - camera_offset.x, j * self.cell_pixel_size - camera_offset.y))
+
 
         # RENDER PLACE BUILDING
         if self.building is not None:
@@ -311,58 +207,21 @@ class GameVue(Scene):
 
             relative_center = self.building_pos // self.cell_pixel_size
             for point in self.building.points:
-                absolute_point = (
-                                         relative_center + point
-                                 ) * self.cell_pixel_size - camera_offset
-                pygame.draw.rect(
-                    self.screen,
-                    self.colors[
-                        (
-                            Colors.BLACK
-                            if self.map.occupied_coords.get(
-                                point + self.building.coords, None
-                            )
-                               is None
-                            else Colors.RED
-                        )
-                    ],
-                    (
-                        absolute_point.x,
-                        absolute_point.y,
-                        self.cell_pixel_size,
-                        self.cell_pixel_size,
-                    ),
-                )
+                absolute_point = (relative_center + point) * self.cell_pixel_size - camera_offset
+                pygame.draw.rect(self.screen, self.colors[Colors.BLACK if self.map.occupied_coords.get(point + self.building.coords, None) is None else Colors.RED], (absolute_point.x, absolute_point.y, self.cell_pixel_size, self.cell_pixel_size))
+
 
         # RENDER BUILDINGS
         # TODO : fix the offset
         for x in range(tl_chunk.x, tl_chunk.x + chunks_size.x + 1):
             for y in range(tl_chunk.y, tl_chunk.y + chunks_size.y + 1):
-                actual_chunk_occupied_coords = self.map.chunk_occupied_coords.get(
-                    Point(x, y), None
-                )
+                actual_chunk_occupied_coords = self.map.chunk_occupied_coords.get(Point(x, y), None)
                 if actual_chunk_occupied_coords is not None:
                     for point in actual_chunk_occupied_coords:
                         struct = self.map.occupied_coords.get(point, None)
-                        if (
-                                struct is not None
-                                and struct.structure_type == StructureType.BUILDING
-                        ):
-                            absolute_point = (
-                                    (point - camera_cell) * self.cell_pixel_size
-                                    + screen_center
-                                    - camera_offset
-                            )
-                            pygame.draw.rect(
-                                self.screen,
-                                self.colors[Colors.BLACK],
-                                (
-                                    absolute_point.x,
-                                    absolute_point.y,
-                                    self.cell_pixel_size,
-                                    self.cell_pixel_size,
-                                ),
-                            )
+                        if struct is not None and struct.structure_type == StructureType.BUILDING:
+                            absolute_point = (point - camera_cell) * self.cell_pixel_size + screen_center - camera_offset
+                            pygame.draw.rect(self.screen, self.colors[Colors.BLACK], (absolute_point.x, absolute_point.y, self.cell_pixel_size, self.cell_pixel_size))
 
     def render_interface(self):
         # TODO : store the map under the interface in a buffer to refresh only the interface when needed and not the entire map
@@ -377,32 +236,19 @@ class GameVue(Scene):
         self.screen.blit(self.ressource_background, (0, offset))
 
         i = 0
-        for ressource_type in [
-            (RessourceType.FOOD, RessourceType.COPPER),
-            (RessourceType.WOOD, RessourceType.IRON),
-            (RessourceType.STONE, RessourceType.CRYSTAL),
-            (RessourceType.GOLD, RessourceType.VULCAN),
-        ]:
+        for ressource_type in [(RessourceType.FOOD, RessourceType.COPPER), (RessourceType.WOOD, RessourceType.IRON), (RessourceType.STONE, RessourceType.CRYSTAL), (RessourceType.GOLD, RessourceType.VULCAN)]:
             ressource_1, ressource_2 = ressource_type
 
-            ressource_icon_1, ressource_text_1 = ressource_icons_texts.get(
-                ressource_1, (None, None)
-            )
-            ressource_icon_2, ressource_text_2 = ressource_icons_texts.get(
-                ressource_2, (None, None)
-            )
+            ressource_icon_1, ressource_text_1 = ressource_icons_texts.get(ressource_1, (None, None))
+            ressource_icon_2, ressource_text_2 = ressource_icons_texts.get(ressource_2, (None, None))
 
-            self.screen.blit(ressource_text_1, (80, offset + 50 + 30 * i))
+            self.screen.blit(ressource_text_1, (80 , offset + 50 + 30 * i))
             self.screen.blit(ressource_text_2, (200, offset + 50 + 30 * i))
 
-            self.screen.blit(ressource_icon_1, (40, offset + 43 + 30 * i))
+            self.screen.blit(ressource_icon_1, (40 , offset + 43 + 30 * i))
             self.screen.blit(ressource_icon_2, (160, offset + 43 + 30 * i))
 
             i += 1
 
     def render_ressource_text(self, ressource_type):
-        return self.ressource_font.render(
-            str(int(self.player.get_ressource(ressource_type))),
-            True,
-            self.colors[Colors.WHITE],
-        )
+        return self.ressource_font.render(str(int(self.player.get_ressource(ressource_type))), True, self.colors[Colors.WHITE])
